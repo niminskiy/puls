4123b86e7d0ef45bf18045bf7430e36c0a3a5150
niminskiy <s.niminskiy@gmail.com>
2020-12-10 20:45:18 +0300

    before site

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'Sergey Niminskiy';                 // Наш логин
+$mail->Username = 's.niminskiy@gmail.com';                 // Наш логин
 $mail->Password = '1HJ39B6N';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
 $mail->setFrom('s.niminskiy@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('s.niminskiy@gmail.com');     // Add a recipient
+$mail->addAddress('s.niminskiy@mail.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
