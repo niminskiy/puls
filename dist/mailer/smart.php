1e591734626edc247d9b59ee835490f142e9069c
niminskiy <s.niminskiy@gmail.com>
2020-12-12 14:04:47 +0300

    mail

--- dist/mailer/smart.php
+++ dist/mailer/smart.php
@@ -8,27 +8,27 @@ require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+// $mail->SMTPDebug = 3;                             // Enable verbose debug output
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 's.niminskiy@gmail.com';                 // Наш логин
-$mail->Password = '1HJ39B6N';                           // Наш пароль от ящика
+$mail->isSMTP();                                     // Set mailer to use SMTP
+$mail->Host = 'smtp.gmail.com';                        // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                              // Enable SMTP authentication
+$mail->Username = 's.niminskiy';             // Наш логин
+$mail->Password = '1HJ39B6N';                         // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('s.niminskiy@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('s.niminskiy@gmail.com');     // Add a recipient
-$mail->addAddress('s.niminskiy@mail.ru');               // Name is optional
-// $mail->addAddress('admin@niminskiy.ru');               // Name is optional
-// $mail->addAddress('niminskiy@niminskiy.ru');               // Name is optional
+$mail->setFrom('s.niminskiy@gmail.com', 'Pulse');       // От кого письмо 
+$mail->addAddress('s.niminskiy@gmail.com');           // Add a recipient
+$mail->addAddress('s.niminskiy@mail.ru');             // Name is optional
+$mail->addAddress('admin@niminskiy.ru');               // Name is optional
+$mail->addAddress('niminskiy@niminskiy.ru');           // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
 //$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
 //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->isHTML(true);                                  	// Set email format to HTML
 
 $mail->Subject = 'Данные';
 $mail->Body    = '
